3df2d32af1fd8d6ac73965c70cbe4e13afe0cc4c
nm <107454696+Natu-ja@users.noreply.github.com>
2023-10-16 17:07:00 +0900

    Add files via upload

---
+++ ABC324/A_Weak_Beats.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc323/tasks/abc323_a
+
+text = list(input())
+
+start, stop, step = 2, 16, 2
+
+for i in range(start, stop+step, step):
+    if int(text[i-1]) == 1:
+        print('No')
+        break
+    else:
+        if i == stop:
+            print('Yes')
